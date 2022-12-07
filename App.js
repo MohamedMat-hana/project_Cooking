dd8695c2617f4e10c738b9bf2f3d05ebd0d37d5f
MohamedMat-hana <MohamedMat-hana>
2022-12-07 11:15:50 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+export class App extends React.Component {
+  render() {
+    return (
+
+
+<Text>ffffdf</Text>
+    );
+  }
+}
+const styles = StyleSheet.create({});
+// https://generation3.000webhostapp.com/project/Training/photographer_list.php
+
+export default App;
