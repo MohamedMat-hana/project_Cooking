1b9ab410b0afb4de936f3c4c5f4a8fd3c436ca54
MohamedMat-hana <MohamedMat-hana>
2022-12-28 00:34:29 +0200

    message

--- src/navigation/HomeStack.js
+++ src/navigation/HomeStack.js
@@ -1,103 +1,86 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { COLORS } from '../constants';
 import Dessert from '../screen/Dessert'
 import Drink from '../screen/Drink'
 import Food from '../screen/Food'
- import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
- const Tab = createMaterialBottomTabNavigator();
+import Index from '../screen/index'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+const Tab = createBottomTabNavigator();
 
 export default HomeStack = () => {
   return (
     <Tab.Navigator
-      initialRouteName="Feed"
-      activeColor="#e91e63"
-      barStyle={{ backgroundColor: 'tomato' }}
+      // initialRouteName="Feed"
+      // activeColor="#e91e63"
+      // barStyle={{ backgroundColor: 'tomato' }}
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.orange,
+        tabBarInactiveTintColor: COLORS.black,
+        tabBarStyle: styles.tabbar,
+        tabBarIcon: ({ color, size, focused }) => {
+          let iconName;
+          if (route.name === Index.Food) {
+            iconName = focused ? "fast-food" : "fast-food-outline"
+          } else if (route.name === Index.Drink) {
+            iconName = focused ? "cafe" : "cafe-outline"
+          } else if (route.name === Index.Dessert) {
+            iconName = focused ? "ice-cream" : "ice-cream-outline"
+          }
+          return <Ionicons name={iconName} size={22} color={color} />
+        }
+      })}
     >
       <Tab.Screen
-        name="Feed"
+        name={Index.Food}
         component={Food}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
+      options={{
+        tabBarLabel: 'Food',
+        // options={{
+          // tabBarButton:props=><
+        // }}
+        // tabBarIcon: ({ color }) => (
+        //   <MaterialCommunityIcons name="home" color={color} size={26} />
+        // ),
+      }}
       />
       <Tab.Screen
-        name="Notifications"
+        name={Index.Dessert}
         component={Dessert}
-        options={{
-          tabBarLabel: 'Updates',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={26} />
-          ),
-        }}
+      // options={{
+      //   tabBarLabel: 'Updates',
+      //   tabBarIcon: ({ color }) => (
+      //     <MaterialCommunityIcons name="bell" color={color} size={26} />
+      //   ),
+      // }}
       />
       <Tab.Screen
-        name="Profile"
+        name={Index.Drink}
         component={Drink}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="account" color={color} size={26} />
-          ),
-        }}
+      // options={{
+      //   tabBarLabel: 'Profile',
+      //   tabBarIcon: ({ color }) => (
+      //     <MaterialCommunityIcons name="account" color={color} size={26} />
+      //   ),
+      // }}
       />
     </Tab.Navigator>
   );
 
-  // return (
-  //   <Tab.Navigator
-  //     inactiveColor={COLORS.blackModal}
-  //     activeColor={COLORS.white}
-  //     barStyle={{ backgroundColor: COLORS.background }}
-  //     >
-  //     <Tab.Screen
-  //       name="Food"
-  //       component={Food}
-  //       options={{
-  //         // tabBarColor: COLORS.background,
-  //         tabBarIcon: ({ color }) => {
-  //           return (
-  //             <View>
-  //               <FontAwesome5 name="ussunnah" color={color} size={24} />
-  //             </View>
-  //           );
-  //         },
-  //       }}
-  //     />
-  //     <Tab.Screen
-  //       name="Dessert"
-  //       component={Dessert}
-  //       options={{
-  //         tabBarIcon: ({ color }) => {
-  //           return (
-  //             <View>
-  //               <FontAwesome5 name="hands" size={20} color={color} />
-  //             </View>
-  //           );
-  //         },
-  //       }}
-  //     />
-  //     <Tab.Screen
-  //       name="Drink"
-  //       component={Drink}
-  //       options={{
-  //         // tabBarColor: COLORS.Yellow,
-  //         tabBarIcon: ({ color }) => {
-  //           return (
-  //             <View>
-  //               <Ionicons name="moon" color={color} size={20} />
-  //             </View>
-  //           );
-  //         },
-  //       }}
-  //     />
- 
-  //   </Tab.Navigator>
-  // );
+
 };
+
+const styles = StyleSheet.create({
+  tabbar: {
+    position: "absolute",
+    backgroundColor:COLORS.ButtonWhite,
+    borderTopWidth:0,
+    buttom:15,
+    right:10,
+    left:10
+  }
+})
---
+++ src/screen/index.js
@@ -0,0 +1,9 @@
+// import Dessert from './Dessert';
+// import Food from './Food';
+// import Drink from './Drink';
+
+export default {
+Dessert:"Dessert",
+Food:"Food",
+Drink:"Drink"
+};
